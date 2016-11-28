7e3068b9b955ffe7ef8b94953347291b91a9f2d8
galsang <kimtaeuk1000@naver.com>
2016-11-28 22:36:25 +0900

    divide classifier modified and preprocessor

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,37 +1,11 @@
 
+import sys
 import json
 import nltk
 from nltk.corpus import stopwords
 from sklearn import svm
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
-
-swords = stopwords.words("english")
-bag_of_words = {}
-top = 10000
-threshold = 5
-json_docs = []
-
-def preprocess_data(limit=100):
-  c = 0
-
-  with open("dataset1.json","r") as f:
-    with open("data.json","w") as f2:
-      while c < limit:
-        c += 1
-        line = f.readline()
-        if line == "":
-          break
-        else:
-          json_line = json.loads(line)
-    
-        tokens = nltk.word_tokenize(json_line["text"])
-        tokens = [token.lower() for token in tokens]
-        unique_tokens = list(set(list(tokens)))
-        words = [dict([(token, tokens.count(token))]) for token in unique_tokens]
-        json_line["sentiment"] = int((json_line["sentiment"] + 1) > 0)
-        output = dict([("serial", json_line["serial"]), ("sentiment", json_line["sentiment"]), ("words", words)])
-        print(json.JSONEncoder().encode(output),file=f2)
 
 def SVM(X,y,tX):
   clf = svm.SVC(kernel="linear")
@@ -51,57 +25,106 @@ def logistic_regression(X,y,tX):
 
   return clf.predict(tX)
 
-def main():
-  with open("data.json","r") as f:
-    while True:
-      line = f.readline()
-      if line == "":
-        break
-      else:
-        json_docs.append(json.loads(line))
+def classify(classifier,X,y,tX,ty):
+  if classifier == "NB":
+    pred = naive_bayesian(X,y,tX)
+  elif classifier == "LR":
+    pred = logistic_regression(X,y,tX)
+  else:
+    pred = SVM(X,y,tX)
+
+  return pred.tolist()
+
+def make_Y(json_docs):
+  return [int(doc["sentiment"]) for doc in json_docs]
+
+def make_X(json_docs,voca):
+  X = []
+  for doc in json_docs:
+    words = [w for word in doc["text"] for w,c in word.items()]
+    x = [1 if v in words else 0 for v in voca]
+    X.append(x)
+
+  return X
+
+def make_voca(bag_of_words,top=10000):
+  swords = stopwords.words("english")
+
+  voca = list(sorted(bag_of_words, key=bag_of_words.__getitem__, reverse=True))
+  voca = [w for w in voca if w not in swords]
+  #threshold = 5
+  #voca = [w for w,c in bag_of_words.items() if c >= threshold and w not in swords]
+  if len(voca) > top:
+    voca = voca[:top]
+
+  return voca
+
+def make_BOW(json_docs):
+  bag_of_words = {}
 
   for doc in json_docs:
-    for word in doc["words"]:
+    for word in doc["text"]:
       for w,c in word.items():
         if w in bag_of_words.keys():
           bag_of_words[w] += c
         else:
           bag_of_words[w] = c
 
-  #voca = [w for w,c in bag_of_words.items() if c >= threshold and w not in swords]
-  voca = list(sorted(bag_of_words, key=bag_of_words.__getitem__, reverse=True))
-  voca = [w for w in voca if w not in swords]
-  if len(voca) > top:
-    voca = voca[:top]
+  return bag_of_words
 
+def write_docs(dic):
+  pass
+
+def read_docs(dic):
+  json_docs = []
+
+  with open(dic,"r") as f:
+    while True:
+      line = f.readline()
+      print(line)
+      if line == "":
+        break
+      else:
+        json_docs.append(json.loads(line))
+
+  return json_docs
+
+def main():
+  if len(sys.argv)>=2 and sys.argv[1] == "-h":
+    print("usage: python classifier.py {path_to_train_data} {path_to_test_data} {path_to_output_json} {classifer=(NB,LR,SVM)")
+    exit(0)
+
+  assert len(sys.argv)>=5, "Please input parameters appropriately. Command python classifier.py -h will help you."
+  train_path = sys.argv[1]
+  test_path = sys.argv[2]
+  output_path = sys.argv[3]
+  classifier = sys.argv[4]
+
+  print(train_path)
+  json_docs = read_docs(train_path)
+  test_json_docs = read_docs(test_path)
+  bag_of_words = make_BOW(json_docs)
+  voca = make_voca(bag_of_words)
+ 
   print("dimension:",len(voca))
+  print("classifier:",classifier)
 
-  X = []
-  for doc in json_docs:
-    words = [w for word in doc["words"] for w,c in word.items()]
-    x = [1 if v in words else 0 for v in voca]
-    X.append(x)
-      
-  y =[int(doc["sentiment"]) for doc in json_docs]
-  
-  boundary = int(limit * 0.8)
-  pred_SVM = SVM(X[:boundary],y[:boundary],X[boundary:limit]).tolist()
-  pred_NB = naive_bayesian(X[:boundary],y[:boundary],X[boundary:limit]).tolist()
-  pred_LR = logistic_regression(X[:boundary],y[:boundary],X[boundary:limit]).tolist()
+  X = make_X(json_docs,voca)
+  y = make_Y(json_docs)
+  tX = make_X(test_json_docs,voca)
+  ty = make_Y(test_json_docs)
+ 
+  results = classify(classifier,X,y,tX,ty)
 
-  answer = y[boundary:limit]
+  accuracy = sum([1 for i,v in enumerate(results) if v == ty[i]])/len(results)
+  print("accuracy:",accuracy)
 
-  accuracy_SVM = sum([1 for i,v in enumerate(pred_SVM) if v == answer[i]])/len(pred_SVM)
-  accuracy_NB = sum([1 for i,v in enumerate(pred_NB) if v == answer[i]])/len(pred_NB)
-  accuracy_LR = sum([1 for i,v in enumerate(pred_SVM) if v == answer[i]])/len(pred_LR)
+  for i, doc in enumerate(test_json_docs):
+    doc["sentiment"] = results[i]
+    del doc["text"]
 
-  #print("prediction:",prediction)
-  #print("answer:",answer)
-  print("SVM accuracy:",accuracy_SVM)
-  print("naive bayes accuracy:",accuracy_NB)
-  print("logistic regression accuracy:",accuracy_LR)
+  with open(output_path,"w") as f:
+    for doc in test_json_docs:
+      print(json.JSONEncoder().encode(doc),file=f)
 
-limit = 10000
-preprocess_data(limit)
-print("data preprocessing finished")
 main()
---
+++ classifier/preprocessor.py
@@ -0,0 +1,33 @@
+
+import sys
+import json
+import nltk
+
+def preprocess_data(input_path, output_path, limit):
+  c = 0
+
+  with open(input_path,"r") as f:
+    with open(output_path,"w") as f2:
+      while c<limit:
+        c+=1
+        line = f.readline()
+        if line == "":
+          break
+        else:
+          json_line = json.loads(line)
+    
+        tokens = nltk.word_tokenize(json_line["text"])
+        tokens = [token.lower() for token in tokens]
+        unique_tokens = list(set(list(tokens)))
+        words = [dict([(token, tokens.count(token))]) for token in unique_tokens]
+        json_line["sentiment"] = int((json_line["sentiment"] + 1) > 0)
+        output = dict([("serial", json_line["serial"]), ("sentiment", json_line["sentiment"]), ("text", words)])
+        print(json.JSONEncoder().encode(output),file=f2)
+
+def main():
+  assert len(sys.argv) >=3, "Please the input & output path"
+  input_path = sys.argv[1] 
+  output_path = sys.argv[2]
+  preprocess_data(input_path, output_path, 120000)
+
+main()
